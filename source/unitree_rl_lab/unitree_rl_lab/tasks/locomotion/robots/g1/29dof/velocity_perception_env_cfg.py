2fc0ac50eb0e2bebe9e82f7ba2d626cd01d40d5c
hqhQAQ <2633116820@qq.com>
2026-04-06 21:13:04 +0800

    消融实验3

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/velocity_perception_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/velocity_perception_env_cfg.py
@@ -46,29 +46,29 @@ ROUGH_TERRAINS_CFG = terrain_gen.TerrainGeneratorCfg(
     slope_threshold=0.75,
     use_cache=False,
     sub_terrains={
-        # # 1. 随机金字塔阶梯
-        # "pyramid_stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
-        #     proportion=0.2,
-        #     step_height_range=(0.0, 0.23), # 下界改为 0.0
-        #     step_width=0.3,
-        #     platform_width=3.0,
-        # ),
-        # # 2. 离散障碍物
-        # "discrete_obstacles": terrain_gen.HfDiscreteObstaclesTerrainCfg(
-        #     proportion=0.2,
-        #     horizontal_scale=0.1,
-        #     vertical_scale=0.005,
-        #     obstacle_height_range=(0.0, 0.2), # 下界改为 0.0
-        #     obstacle_width_range=(1.0, 2.0),
-        #     num_obstacles=40,
-        # ),
-        # # 3. 标准阶梯
-        # "stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
-        #     proportion=0.2,
-        #     step_height_range=(0.0, 0.2), # 下界改为 0.0
-        #     step_width=0.3,
-        #     platform_width=3.0
-        # ),
+        # 1. 随机金字塔阶梯
+        "pyramid_stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
+            proportion=0.2,
+            step_height_range=(0.0, 0.23), # 下界改为 0.0
+            step_width=0.3,
+            platform_width=3.0,
+        ),
+        # 2. 离散障碍物
+        "discrete_obstacles": terrain_gen.HfDiscreteObstaclesTerrainCfg(
+            proportion=0.2,
+            horizontal_scale=0.1,
+            vertical_scale=0.005,
+            obstacle_height_range=(0.0, 0.2), # 下界改为 0.0
+            obstacle_width_range=(1.0, 2.0),
+            num_obstacles=40,
+        ),
+        # 3. 标准阶梯
+        "stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
+            proportion=0.2,
+            step_height_range=(0.0, 0.2), # 下界改为 0.0
+            step_width=0.3,
+            platform_width=3.0
+        ),
         # 4. 坑洼/波浪地面
         "random_rough": terrain_gen.HfWaveTerrainCfg(
             proportion=0.2,
@@ -98,10 +98,6 @@ ROUGH_TERRAINS_CFG = terrain_gen.TerrainGeneratorCfg(
             slope_range=(0.15, 0.25),   # 约 8.6°‑14.3°
             platform_width=3.0,
         ),
-        # "pit": terrain_gen.MeshPitTerrainCfg(
-        #     proportion=0.05,
-        #     pit_depth_range=(0.3, 0.5), # 坑深 30‑50cm
-        # ),
     },
 )
 
@@ -279,16 +275,16 @@ class ObservationsCfg:
         # gait_phase = ObsTerm(func=mdp.gait_phase, params={"period": 0.8})
 
         # 高度扫描（187）
-        # height_scanner = ObsTerm(
-        #     func=mdp.height_scan_hpc,
-        #     params={
-        #         "sensor_cfg": SceneEntityCfg("height_scanner"),
-        #         "offset": 0.78,  # 👈 核心修改：与目标躯干高度对齐，让平地归零
-        #     },
-        #     scale = 1.0,
-        #     clip=(-1.0, 1.0),    # 既然平地归零了，台阶和坑的起伏很少超过 1 米
-        #     noise=Unoise(n_min=-0.1, n_max=0.1) # 根据 base_env_config.py 还原 0.1 的噪声
-        # )
+        height_scanner = ObsTerm(
+            func=mdp.height_scan_hpc,
+            params={
+                "sensor_cfg": SceneEntityCfg("height_scanner"),
+                "offset": 0.78,  # 👈 核心修改：与目标躯干高度对齐，让平地归零
+            },
+            scale = 1.0,
+            clip=(-1.0, 1.0),    # 既然平地归零了，台阶和坑的起伏很少超过 1 米
+            noise=Unoise(n_min=-0.1, n_max=0.1) # 根据 base_env_config.py 还原 0.1 的噪声
+        )
 
         def __post_init__(self):
             self.history_length = 1  # 取消历史帧
@@ -310,11 +306,18 @@ class ObservationsCfg:
         joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel, scale=0.05)
         last_action = ObsTerm(func=mdp.last_action)
         # gait_phase = ObsTerm(func=mdp.gait_phase, params={"period": 0.8})
-        # height_scanner = ObsTerm(
-        #     func=mdp.height_scan,
-        #     params={"sensor_cfg": SceneEntityCfg("height_scanner")},
-        #     clip=(-1.0, 5.0),
-        # )
+        
+        # 高度扫描（187）
+        height_scanner = ObsTerm(
+            func=mdp.height_scan_hpc,
+            params={
+                "sensor_cfg": SceneEntityCfg("height_scanner"),
+                "offset": 0.78,  # 👈 核心修改：与目标躯干高度对齐，让平地归零
+            },
+            scale = 1.0,
+            clip=(-1.0, 1.0),    # 既然平地归零了，台阶和坑的起伏很少超过 1 米
+            noise=Unoise(n_min=-0.1, n_max=0.1) # 根据 base_env_config.py 还原 0.1 的噪声
+        )
 
 
         def __post_init__(self):
